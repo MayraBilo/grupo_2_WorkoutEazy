dfe299c6d3ed28656e2337c066ac7f20c131563d
Jacintamayol94 <jacimayol94@gmail.com>
2023-05-10 22:59:37 -0300

    mayusc archivos

--- app.js
+++ app.js
@@ -4,26 +4,26 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "./Public")));
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "./Views/index.html"))
+  res.sendFile(path.join(__dirname, "./views/index.html"))
 );
 
-app.get("/detalles-del-producto", (req, res) =>
-  res.sendFile(path.join(__dirname, "./Views/productDetail.html"))
+app.get("/productDetail", (req, res) =>
+  res.sendFile(path.join(__dirname, "./views/productDetail.html"))
 );
 
-app.get("/carrito-de-compras", (req, res) =>
-  res.sendFile(path.join(__dirname, "./Views/productCart.html"))
+app.get("/productCart", (req, res) =>
+  res.sendFile(path.join(__dirname, "./views/productCart.html"))
 );
 
 app.get("/register", (req, res) =>
-  res.sendFile(path.join(__dirname, "./Views/register.html"))
+  res.sendFile(path.join(__dirname, "./views/register.html"))
 );
 
 app.get("/login", (req, res) =>
-  res.sendFile(path.join(__dirname, "./Views/login.html"))
+  res.sendFile(path.join(__dirname, "./views/login.html"))
 );
 
 app.listen(3000, () => console.log("Servidor escuchando en el puerto 3000🚀"));
