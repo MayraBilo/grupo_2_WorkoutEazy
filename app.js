d8e68fe9a982a49000ab1dc3ba934527595e9893
Jacintamayol94 <jacimayol94@gmail.com>
2023-05-14 19:08:56 -0300

    correcciones home

--- app.js
+++ app.js
@@ -7,23 +7,23 @@ const app = express();
 app.use(express.static(path.join(__dirname, "./public")));
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "./views/index.html"))
+  res.sendFile(path.join(__dirname, "./views/user/index.html"))
 );
 
 app.get("/productDetail", (req, res) =>
-  res.sendFile(path.join(__dirname, "./views/productDetail.html"))
+  res.sendFile(path.join(__dirname, "./views/productos/productDetail.html"))
 );
 
 app.get("/productCart", (req, res) =>
-  res.sendFile(path.join(__dirname, "./views/productCart.html"))
+  res.sendFile(path.join(__dirname, "./views/productos/productCart.html"))
 );
 
 app.get("/register", (req, res) =>
-  res.sendFile(path.join(__dirname, "./views/register.html"))
+  res.sendFile(path.join(__dirname, "./views/user/register.html"))
 );
 
 app.get("/login", (req, res) =>
-  res.sendFile(path.join(__dirname, "./views/login.html"))
+  res.sendFile(path.join(__dirname, "./views/user/login.html"))
 );
 
 app.listen(3000, () => console.log("Servidor escuchando en el puerto 3000🚀"));
