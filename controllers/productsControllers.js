2bbf637fc5dad3fd7afd031a407815bae4e8ce5a
FranquitoDotado <sebasrodri578@gmail.com>
2023-05-17 23:11:58 -0300

    avance sprint 3

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,10 +1,8 @@
 const path = require("path");
 
 const controller = {
-    getDetail: (req, res) =>
-    res.sendFile(path.join(__dirname, "../views/productos/productDetail.html")),
-    getCart: (req, res) =>
-    res.sendFile(path.join(__dirname, "../views/productos/productCart.html"))
+    getDetail: (req, res) => res.render("productDetail"),
+    getCart: (req, res) => res.render("productCart"),
 };
 
 module.exports = controller;
