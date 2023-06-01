80ecb64ca8d1117248abb54f1e806f3b3c7cc8a5
FranquitoDotado <sebasrodri578@gmail.com>
2023-05-31 22:56:43 -0300

    pre-sprint-4

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,18 +1,35 @@
 const path = require("path");
+const fs = require("fs")
+const productFilePath = path.join(__dirname, "../data/products.json");
+const products = JSON.parse(fs.readFileSync(productFilePath, "utf-8"));
 
 const controller = {
-    getDetail: (req, res) => res.render("productDetail"),
-    getCart: (req, res) => res.render("productCart"),
-    getCreate: (req, res) => res.render("createProduct"),
-    getEdite: (req, res) => res.render("editeProduct"),
-    getService: (req, res) => res.render("profileServices"),
-    getList: (req, res) => res.render("productList"),
-    getListYoga: (req, res) => res.render("productListYoga"),
-    getListFitness: (req, res) => res.render("productListFitness"),
-    getListMontana: (req, res) => res.render("productListMontana"),
-    getListBaile: (req, res) => res.render("productListBaile"),
-    getListFootball: (req, res) => res.render("productListFootball"),
-    getListTennis: (req, res) => res.render("productListTennis"),
+    getList: (req, res) => {
+        res.render("productList", {
+            products: products,
+        })},
+    getDetail: (req, res) => {
+        res.render("productDetail")},
+    getCart: (req, res) => {
+        res.render("productCart")},
+    getCreate: (req, res) => {
+        res.render("createProduct")},
+    getEdite: (req, res) => {
+        res.render("editeProduct")},
+    getService: (req, res) => {
+        res.render("profileServices")},
+    getListYoga: (req, res) => {
+        res.render("productListYoga")},
+    getListFitness: (req, res) => {
+        res.render("productListFitness")},
+    getListMontana: (req, res) => {
+        res.render("productListMontana")},
+    getListBaile: (req, res) => {
+        res.render("productListBaile")},
+    getListFootball: (req, res) => {
+        res.render("productListFootball")},
+    getListTennis: (req, res) => {
+        res.render("productListTennis")},
 };
 
 module.exports = controller;
