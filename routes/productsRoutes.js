854470875d6bc30edfd2a35294838b2d47d1ec8c
elizaLemos <liz0912@hotmail.com>
2023-06-07 10:15:33 -0500

    se aceptan cambios en routes

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -21,55 +21,6 @@ router.get('/product/', productController.getList);
 // @GET /products/:id/detail ---> /products/5/detail
 router.get('/:id/productDetail', productController.getDetail);
 
-// @POST /products
-router.post('/', upload.any('img'), productController.postProduct);
-
-// @GET /products/create
-router.get('/createProduct', productController.getCreate);
-
-// @DELETE /products/:id/delete ---> /products/5/delete
-router.delete('/:id/delete', productController.deleteProduct);
-
-// @GET /products/:id/update 
-router.get('/:id/editeProduct', productController.getEdit);
-
-// @PUT /products/:id/update ---> /products/5/put
-router.put('/:id/editeProduct', productController.updateProduct);
-
-router.get('/productCart', productController.getCart);
-
-router.get('/profileServices', productController.getService);
-
-router.get('/productListYoga', productController.getListYoga);
-router.get('/productListFitness', productController.getListFitness);
-router.get('/productListDeportes', productController.getListDeportes);
-router.get('/productListDanzas', productController.getListDanzas);
-
-
-
-const express = require('express');
-const path = require('path');
-const multer = require('multer');
-
-const productController = require('../controllers/productsControllers');
-
-const router = express.Router();
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './public/imgs/products');
-    },
-    filename: (req, file, cb) => {
-        console.log(path.extname(file.originalname))
-        cb(null, Date.now() + '-' + file.originalname);
-    }
-});
-const upload = multer({ storage });
-//@GET / products
-router.get('/product/', productController.getList);
-
-// @GET /products/:id/detail ---> /products/5/detail
-router.get('/:id/productDetail', productController.getDetail);
-
 
 // @GET /products/create
 router.get('/createProduct', productController.getCreate);
@@ -97,6 +48,3 @@ router.get('/productListDanzas', productController.getListDanzas);
 
 
 
-
-module.exports = router;
-module.exports = router;
