2377bda38ea86c2352be11eb0ecbcd670e9af3f1
FranquitoDotado <sebasrodri578@gmail.com>
2023-06-12 14:47:45 -0300

    arreglo create/edit product

--- app.js
+++ app.js
@@ -1,40 +1,31 @@
+/* const { log } = require("console"); */
 const express = require("express");
 const path = require("path");
-const methodOverride = require("method-override");
+const methodOverride = require('method-override');
 
-const mainRoutes = require("./routes/mainRoutes");
-const productsRoutes = require("./routes/productsRoutes");
-const userRoutes = require("./routes/userRoutes");
+const mainRoutes = require('./routes/mainRoutes');
+const productsRoutes = require('./routes/productsRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "./public")));
 
 app.set("view engine", "ejs");
-
-app.set("views", [
-    path.join(__dirname, "./views/main"),
-    path.join(__dirname, "./views/productos"),
-    path.join(__dirname, "./views/user"),
-]);
-
+    app.set("views", [
+        path.join(__dirname, "./views/main"),
+        path.join(__dirname, "./views/productos"),
+        path.join(__dirname, "./views/user"),
+    ]);
 // --- Middlewares ---
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 
-//----Routes----//
-
+//----routes----//
 app.use(mainRoutes);
-app.use("/product", productsRoutes);
+app.use('/product',productsRoutes);
 app.use(userRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404-error");
-  next();
-});
-
-app.listen(3000, () =>
-  console.log("Servidor escuchando en el puerto http://localhost:3000/")
-);
+app.listen(3000, () => console.log("Servidor escuchando en el puerto http://localhost:3000/"));
