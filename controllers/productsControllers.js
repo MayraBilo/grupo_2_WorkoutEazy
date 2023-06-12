9f6c88ca73c3e85e35c32775f1794a360d9af560
Mayra Bilo <may.bilo92@gmail.com>
2023-06-12 00:28:24 -0300

    actualizacion form createProduct y estilo

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,114 +1,112 @@
 const path = require("path");
-const productModel = require('../models/product');
+const productModel = require("../models/product");
 
 const controller = {
-    getList: (req, res) => {
-        const productos = productModel.findAll();
-        res.render("productList", {
-            products: productos,
-        })
-    },
-    getEdit: (req, res) => {
-        const id = Number(req.params.id);
+  getList: (req, res) => {
+    const productos = productModel.findAll();
+    res.render("productList", {
+      products: productos,
+    });
+  },
+  getEdit: (req, res) => {
+    const id = Number(req.params.id);
 
-        const productoAModificar = productModel.findById(id)
+    const productoAModificar = productModel.findById(id);
 
-        if (!productoAModificar) {
-            return res.send('error de id');
-        }
+    if (!productoAModificar) {
+      return res.send("error de id");
+    }
 
-        res.render("editProduct", {
-            products: productoAModificar
-        });
-    },
-    getDetail: (req, res) => {
-        const id = Number(req.params.id);
+    res.render("editProduct", {
+      products: productoAModificar,
+    });
+  },
+  getDetail: (req, res) => {
+    const id = Number(req.params.id);
 
-        const productoAMostrar = productModel.findById(id);
+    const productoAMostrar = productModel.findById(id);
 
-        if (!productoAMostrar) {
-            return res.send('error de id');
-        }
+    if (!productoAMostrar) {
+      return res.send("error de id");
+    }
 
-        res.render("productDetail", {products: productoAMostrar })
-    },
-    deleteProduct: (req, res) => {
-        const id = Number(req.params.id);
+    res.render("productDetail", { products: productoAMostrar });
+  },
+  deleteProduct: (req, res) => {
+    const id = Number(req.params.id);
 
-        productModel.deleteById(id);
+    productModel.deleteById(id);
 
-        res.redirect('/product');
-    },
-    updateProduct: (req, res) => {
-        const id = Number(req.params.id);
-        const nuevosDatos = req.body;
-        nuevosDatos.img = req.file ? req.file.filename : req.body.oldImage
+    res.redirect("/product");
+  },
+  updateProduct: (req, res) => {
+    const id = Number(req.params.id);
+    const nuevosDatos = req.body;
+    nuevosDatos.img = req.file ? req.file.filename : req.body.oldImage;
 
-        productModel.updateById(id, nuevosDatos);
+    productModel.updateById(id, nuevosDatos);
 
-        res.redirect('/product');
-    },
-    getCart: (req, res) => {
-        let products = productModel.findAll()
-        res.render("productCart", {
-            products: products})
-    },
-    getCreate: (req, res) => {
-        res.render("createProduct")
-    },
-    getService: (req, res) => {
-        res.render("profileServices")
-    },
+    res.redirect("/product");
+  },
+  getCart: (req, res) => {
+    let products = productModel.findAll();
+    res.render("productCart", {
+      products: products,
+    });
+  },
+  getCreate: (req, res) => {
+    res.render("createProduct");
+  },
+  getService: (req, res) => {
+    res.render("profileServices");
+  },
 
-    getListYoga: (req, res) => {
-        const productos = productModel.findAll();
-        res.render("productListYoga", {
-            products: productos,
-        })
-    },
+  getListYoga: (req, res) => {
+    const productos = productModel.findAll();
+    res.render("productListYoga", {
+      products: productos,
+    });
+  },
 
-    getListFitness: (req, res) => {
-        const productos = productModel.findAll();
-        res.render("productListFitness", {
-            products: productos,
-        })
-    },
+  getListFitness: (req, res) => {
+    const productos = productModel.findAll();
+    res.render("productListFitness", {
+      products: productos,
+    });
+  },
 
-    getListDeportes: (req, res) => {
-        const productos = productModel.findAll();
-        res.render("productListDeportes", {
-            products: productos,
-        })
-    },
+  getListDeportes: (req, res) => {
+    const productos = productModel.findAll();
+    res.render("productListDeportes", {
+      products: productos,
+    });
+  },
 
-    getListDanzas: (req, res) => {
-        const productos = productModel.findAll();
-        res.render("productListDanzas", {
-            products: productos,
-        })
-    },
+  getListDanzas: (req, res) => {
+    const productos = productModel.findAll();
+    res.render("productListDanzas", {
+      products: productos,
+    });
+  },
 
-    postProduct: (req, res) => {
-        
-        let datos = req.body;
+  postProduct: (req, res) => {
+    let datos = req.body;
 
-        datos.id = productModel.length + 1;
+    datos.id = productModel.length + 1;
 
-        datos.valor = Number(datos.valor);
-       
-        /*datos.img = req.files.map(file => `/images/productos/${file.filename}`);*/
-        datos.img = req.file ? req.file.filename : 'sin foto'
+    datos.valor = Number(datos.valor);
 
+    /*datos.img = req.files.map(file => `/images/productos/${file.filename}`);*/
+    datos.img = req.file ? req.file.filename : "sin foto";
 
-        productModel.createOne(datos); 
+    productModel.createOne(datos);
 
-        res.redirect('/product');
-        res.redirect('/productListDanzas')
-        res.redirect('/productListDeportes')
-        res.redirect('/productListFitness')
-        res.redirect('/productListYoga')
-    },
-
+    res.redirect("/product");
+    res.redirect("/productListDanzas");
+    res.redirect("/productListDeportes");
+    res.redirect("/productListFitness");
+    res.redirect("/productListYoga");
+  },
 };
 
 module.exports = controller;
