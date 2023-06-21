753a316b32ecb96139588f08f9a6c343bd3b309c
Mayra Bilo <may.bilo92@gmail.com>
2023-06-20 22:14:36 -0300

    estructura de archivos users, models y planteo (sin funcionalidad)  de routes/controllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,9 +1,18 @@
 const path = require("path");
 
 const controller = {
-    getLogin: (req, res) => res.render("login"),
-    getRegister: (req, res) => res.render("register"),
-    getRegisterAliados: (req, res) => res.render("registerAliados"),
+  getLogin: (req, res) => res.render("login"),
+  getRegister: (req, res) => res.render("register"),
+  getRegisterAliados: (req, res) => res.render("registerAliados"),
+  postLogin: (req, res) => {
+    console.log("acá va a ir el código para el post login");
+  },
+  postRegister: (req, res) => {
+    console.log("acá va a ir el código para el post register");
+  },
+  postRegisterAliados: (req, res) => {
+    console.log("acá va a ir el código para el post registerAliados");
+  },
 };
 
 module.exports = controller;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,17 @@
-const express = require('express');
+const express = require("express");
 
-const userControllers = require('../controllers/userControllers');
+const userControllers = require("../controllers/userControllers");
 
 const router = express.Router();
 
-router.get('/login', userControllers.getLogin);
-router.get('/register', userControllers.getRegister);
-router.get('/registerAliados', userControllers.getRegisterAliados);
+router.get("/login", userControllers.getLogin);
+
+router.post("/", userControllers.postLogin);
+
+router.get("/register", userControllers.getRegister);
+router.post("/", userControllers.postRegister);
+
+router.get("/registerAliados", userControllers.getRegisterAliados);
+router.post("/", userControllers.postRegisterAliados);
 
 module.exports = router;
