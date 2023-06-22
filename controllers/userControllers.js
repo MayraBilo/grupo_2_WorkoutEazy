675ded0f529158b9f2b42e3f893166d7ce47412f
Jacintamayol94 <jacimayol94@gmail.com>
2023-06-22 16:19:24 -0300

    register funcionando con hash

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,8 +1,42 @@
 const path = require("path");
 
+const user = require('../models/cliente');
+
+const bcrypt = require('bcryptjs');
+
 const controller = {
     getLogin: (req, res) => res.render("login"),
     getRegister: (req, res) => res.render("register"),
+
+    processRegister: (req, res) => {
+
+        /*const resultValidation = validationResult(req);
+        
+        if (resultValidation.errors.length > 0) {
+            return res.render('register', {
+                errors: resultValidation.mapped(),
+                oldData: req.body
+            })
+        }
+
+        let userInDB = user.findByField('email', req.body.email);
+
+        if (userInDB) {
+            return res.render('register', {
+                errors: {email: {msg: 'Este email ya está registrado'}},
+                oldData: req.body})
+        }*/
+
+        let userToCreate = {
+            ...req.body,
+            password: bcrypt.hashSync(req.body.password, 10),
+            avatar: req.file.filename
+        }
+
+        let userCreated = user.create(userToCreate);
+        return res.redirect('/login')
+},
+
     getRegisterAliados: (req, res) => res.render("registerAliados"),
     clientProfile: (req, res) => {res.render("perfilCliente")}
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 
+const router = express.Router();
+
+const path = require ('path');
+
+// Controller
+
 const userControllers = require("../controllers/userControllers");
 
-const router = express.Router();
+// Middlewares
+const uploadFile = require('../middlewares/multerMiddleware.js');
+
 
 // Formulario de login
 router.get('/login', userControllers.getLogin);
@@ -10,6 +18,9 @@ router.get('/login', userControllers.getLogin);
 // Formulario de registro
 router.get('/register', userControllers.getRegister);
 router.get('/registerAliados', userControllers.getRegisterAliados);
+
+// Procesar el registro
+router.post('/register', uploadFile.single('avatar'), userControllers.processRegister);
 
 // Perfil de usuario
 
