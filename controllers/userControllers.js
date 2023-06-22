15e831d0752490c4c73b1a9d0b0b4ff91ad23054
elizaLemos <liz0912@hotmail.com>
2023-06-22 09:14:07 -0500

    Creación página de perfil Aliado

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,6 +4,7 @@ const controller = {
     getLogin: (req, res) => res.render("login"),
     getRegister: (req, res) => res.render("register"),
     getRegisterAliados: (req, res) => res.render("registerAliados"),
+    getProfileAliados: (req, res) => res.render("profileAliados"),
 };
 
 module.exports = controller;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/login', userControllers.getLogin);
 router.get('/register', userControllers.getRegister);
 router.get('/registerAliados', userControllers.getRegisterAliados);
+router.get('/profileAliados', userControllers.getProfileAliados);
 
 module.exports = router;
