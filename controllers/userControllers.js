72f95146327f24c55f7c14b900f0e4c801073295
Jacintamayol94 <jacimayol94@gmail.com>
2023-06-22 10:42:15 -0300

    vista perfil cliente

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,6 +4,7 @@ const controller = {
     getLogin: (req, res) => res.render("login"),
     getRegister: (req, res) => res.render("register"),
     getRegisterAliados: (req, res) => res.render("registerAliados"),
+    clientProfile: (req, res) => {res.render("perfilCliente")}
 };
 
 module.exports = controller;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,8 +4,15 @@ const userControllers = require('../controllers/userControllers');
 
 const router = express.Router();
 
+// Formulario de login
 router.get('/login', userControllers.getLogin);
+
+// Formulario de registro
 router.get('/register', userControllers.getRegister);
 router.get('/registerAliados', userControllers.getRegisterAliados);
+
+// Perfil de usuario
+
+router.get('/perfilCliente', userControllers.clientProfile);
 
 module.exports = router;
