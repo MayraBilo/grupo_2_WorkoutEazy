54dafe0d5e6675a14fe3d7a59b5ef551073ea8b0
elizaLemos <liz0912@hotmail.com>
2023-08-07 23:09:31 -0500

    Cambios en validaciones frontEnd registerAliados

--- public/js/formRegistroAliado.js
+++ public/js/formRegistroAliado.js
@@ -9,6 +9,7 @@ const contact_numberInp = document.querySelector('#contact_number');
 const emailInp = document.querySelector('#email');
 const passwordInp = document.querySelector('#password');
 const avatarInp = document.querySelector('#avatar');
+const enviarBtn = document.getElementById("enviar");
 
 const setErrors = (message, inp, isError = true) => {
     if (isError) {
@@ -22,7 +23,8 @@ const setErrors = (message, inp, isError = true) => {
     }
   }
 
-//Validaciones con Expresiones regulares
+//Validaciones con Expresiones regulares:
+
 const validateEmptyInp = (message, e) => {
     const inp = e.target;
     const inpValue = e.target.value;
@@ -33,20 +35,7 @@ const validateEmptyInp = (message, e) => {
       setErrors("", inp, false);
     }
   }
-/*const validateEntiInp = (message, e) => {
-    const inp = e.target;
-    const inpValue = e.target.value;
 
-    if (inpValue.trim().length === 0) {
-        inp.classList.add("invalid");
-        inp.nextElementSibling.classList.add("error");
-        inp.nextElementSibling.innerText = message;
-    } else {
-        inp.classList.remove("invalid");
-        inp.nextElementSibling.classList.remove("error");
-        inp.nextElementSibling.innerText = " ";
-    }
-};*/
 const validateNombreInp = e => {
     const inp = e.target;
     const inpValue = e.target.value;
@@ -57,7 +46,18 @@ const validateNombreInp = e => {
     } else {
         setErrors("", inp, false);
     }
-};
+}; 
+const validateBirthdayInp = e => {
+    const inp = e.target;
+    const inpValue = e.target.value;
+    //const regex = new RegExp(/^[a-zA-ZÀ-ÿ\s]{3,40}$/); 
+
+    if (inpValue.trim().length === 0 ) {
+        setErrors("Ingresa tus fecha de Nacimiento", inp);
+    } else {
+        setErrors("", inp, false);
+    }
+}; 
 
 const validateServiceInp = e => {
     const inp = e.target;
@@ -65,116 +65,105 @@ const validateServiceInp = e => {
     const regex = new RegExp(/^[a-zA-ZÀ-ÿ\s]{6,40}$/); 
 
     if (inpValue.trim().length =! regex.test(e.target.value) ) {
-        inp.classList.add("invalid");
-        inp.nextElementSibling.classList.add("error");
-        inp.nextElementSibling.innerText = "Ingresa tu servicio, de 6 a 30 caracteres";
+        setErrors("Ingresa tu servicio, de 6 a 30 caracteres", inp);
     } else {
-        inp.classList.remove("invalid");
-        inp.nextElementSibling.classList.remove("error");
-        inp.nextElementSibling.innerText = "";
+        setErrors("", inp, false);
     }
-};
+}; 
 const validateDocument_numberInp = e => {
     const inp = e.target;
-    const inpValueDocument = e.target.value;
+    const inpValue = e.target.value;
     const regex = new RegExp(/^\d{11,13}$/);
 
-    if (!regex.test(inpValueDocument)){
-        inp.classList.add("invalid");
-        inp.nextElementSibling.classList.add("error");
-        inp.nextElementSibling.innerText = "Número documento inválido";
+    if (inpValue.trim().length =! regex.test(e.target.value) ) {
+        setErrors("Número documento inválido", inp);
     } else {
-        inp.classList.remove("invalid");
-        inp.nextElementSibling.classList.remove("error");
-        inp.nextElementSibling.innerText = "";
+        setErrors("", inp, false);
     }
-
-};
+}; 
 const validateContac_numberInp = e => {
     const inp = e.target;
-    const inpValueContact = e.target.value;
+    const inpValue = e.target.value;
     const regex = new RegExp(/^\d{10}$/);
 
-    if (!regex.test(inpValueContact)){
-        //(inpValueContact.trim().length =! regex.test(e.target.value))
-        inp.classList.add("invalid");
-        inp.nextElementSibling.classList.add("error");
-        inp.nextElementSibling.innerText = "Número celular inválido";
+    if (inpValue.trim().length =! regex.test(e.target.value) ) {
+        setErrors("Número celular inválido", inp);
     } else {
-        inp.classList.remove("invalid");
-        inp.nextElementSibling.classList.remove("error");
-        inp.nextElementSibling.innerText = "";
+        setErrors("", inp, false);
     }
 
 };
 
 const validateEmailFormat = e => {
     const inp = e.target;
-    const inpValueEmail = e.target.value;
+    const inpValue = e.target.value;
     const regex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
 
-    if (!regex.test(inpValueEmail)) {
-        inp.classList.add("invalid");
-        inp.nextElementSibling.classList.add("error");
-        inp.nextElementSibling.innerText = "Ingresa email válido";
+    if (inpValue.trim().length =! regex.test(e.target.value) ) {
+        setErrors("Ingresa email válido", inp);
     } else {
-        inp.classList.remove("invalid");
-        inp.nextElementSibling.classList.remove("error");
-        inp.nextElementSibling.innerText = "";
+        setErrors("", inp, false);
     }
-}
+} 
 
 const validatePasswordFormat = e => {
     const inp = e.target;
-    const inpValuePassword = e.target.value;
+    const inpValue = e.target.value;
     const regex = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*]{8,}$/);
 
-    if (!regex.test(inpValuePassword)) {
-        inp.classList.add("invalid");
-        inp.nextElementSibling.classList.add("error");
-        inp.nextElementSibling.innerText = "Utiliza mímino 8 caracteres, con al menos una letra mayúscula, números y símbolos";
+    if (inpValue.trim().length =! regex.test(e.target.value) ) {
+        setErrors("Utiliza mímino 8 caracteres, con al menos una letra mayúscula, números y símbolos", inp);
     } else {
-        inp.classList.remove("invalid");
-        inp.nextElementSibling.classList.remove("error");
-        inp.nextElementSibling.innerText = "";
-}
+        setErrors("", inp, false);
+    }
 }
 
-//Implementación de validaciones con Expresiones regulares
-emailInp.addEventListener('input', validateEmailFormat);
-passwordInp.addEventListener('input', validatePasswordFormat);
+//Implementación de validaciones con Expresiones regulares:
+
+emailInp.addEventListener('blur', validateEmailFormat);
+passwordInp.addEventListener('blur', validatePasswordFormat);
 entity_nameInp.addEventListener('blur', validateNombreInp);
 first_nameInp.addEventListener('blur', validateNombreInp);
 last_nameInp.addEventListener('blur', validateNombreInp);
 servicesInp.addEventListener('blur', validateServiceInp);
+document_numberInp.addEventListener('blur', validateDocument_numberInp);
+contact_numberInp.addEventListener('blur', validateContac_numberInp);
+birth_dateInp.addEventListener('blur', validateBirthdayInp);
+
+//validación datos completados exitosamente
+emailInp.addEventListener('input', validateEmailFormat);
+passwordInp.addEventListener('input', validatePasswordFormat);
+entity_nameInp.addEventListener('input', validateNombreInp);
+first_nameInp.addEventListener('input', validateNombreInp);
+last_nameInp.addEventListener('input', validateNombreInp);
+servicesInp.addEventListener('input', validateServiceInp);
 document_numberInp.addEventListener('input', validateDocument_numberInp);
 contact_numberInp.addEventListener('input', validateContac_numberInp);
-
-//Validaciones de espacio vacío
-emailInp.addEventListener('blur', (e) => validateEntiInp("Ingresa información requerida", e));
-passwordInp.addEventListener('blur', (e) => validateEntiInp("Ingresa contraseña requerida", e));
-document_numberInp.addEventListener('blur', (e) => validateEntiInp("Ingresa información requerida", e));
-contact_numberInp.addEventListener('blur', (e) => validateEntiInp("Ingresa número celular", e));
-birth_dateInp.addEventListener('blur', (e) => validateEntiInp("Ingresa fecha de nacimiento", e));
-services_cityInp.addEventListener('blur', (e) => validateEntiInp("Ingresa ciudad de servicios", e));
+birth_dateInp.addEventListener('input', validateBirthdayInp);
 
 //Validación de archivo para foto perfil
 
-avatarInp.addEventListener('input', (e) => validateEntiInp("Subir foto de perfil", e));
-
 avatarInp.addEventListener('blur', (e) => {
-    const inp = e.target;
-    const avatarExt = e.target.files[0].name.split(".").pop().toLowerCase();
-    const allowedExt = ["jpg", "jpeg", "png", "gif"];
-    if (!allowedExt.includes(avatarExt)) {
-        inp.classList.add("invalid");
-        inp.nextElementSibling.classList.add("error");
-        inp.nextElementSibling.innerText = "Adjunta un archivo válido: .jpg, .jpeg, .png, .gif";
-    } else {
-        inp.classList.remove("invalid");
-        inp.nextElementSibling.classList.remove("error");
-        inp.nextElementSibling.innerText = "";
-    }
+    const ind = e.target;
+  const fileExt = e.target.files[0].name.split(".").pop().toLowerCase();
+  const allowedExt = ["jpg", "jpeg", "png", "gif"];
+  if (!allowedExt.includes(fileExt)) {
+    setErrors ("Adjunta un archivo válido: .jpg, .jpeg, .png, .gif", ind);
+  } else {
+    setErrors("", ind, false);
+  }
 });
 
-  
+//Deshabilitar submit si no se llenan los datos
+
+/*function validarCampos() {
+    if (entity_nameInp.value.trim() !== "" && first_nameInp.value.trim() !== "" && last_nameInp.value.trim() !== "" && servicesvalue.trim() !== "" && servicesInp.value.trim() !== "" 
+    && document_numberInp.value.trim() !== "" && birth_dateInp.trim() !== "" && services_cityInp.value.trim() !== "" && contact_numberInp.value.trim() !== "" && emailInp.value.trim() !== "" 
+    && passwordInp.value.trim() !== "" && avatarInp.value.trim() !== "") {
+        enviarBtn.removeAttribute("disabled");
+    } else {
+        enviarBtn.setAttribute("disabled", "true");
+    }
+}
+*/
+
