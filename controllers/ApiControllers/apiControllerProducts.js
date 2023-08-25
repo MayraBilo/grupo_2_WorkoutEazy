8e2e29416d4b7d023f7318639eb25121924e7aea
Jacintamayol94 <jacimayol94@gmail.com>
2023-08-25 10:19:03 -0300

    image pending

--- controllers/ApiControllers/apiControllerProducts.js
+++ controllers/ApiControllers/apiControllerProducts.js
@@ -11,12 +11,12 @@ const controller = {
             const fitness = await db.Producto.findAll({ where: { category: 'Fitness' } })
             const deportes = await db.Producto.findAll({ where: { category: 'Deportes' } })
             const aliadoProducto = await db.Producto.findAll({ include: [{ association: "aliado_producto" }] })
-            const arrayRelacion = aliadoProducto.map( relacion => {
+            const arrayRelacion = aliadoProducto.map(relacion => {
                 return {
                     nombre: relacion.activity_name,
                     aliado: relacion.aliado_producto
                 }
-                } )
+            })
 
             return res.status(200).json({
                 count: allProducts.length,
@@ -40,30 +40,27 @@ const controller = {
             });
         }
     },
-    getApiDetail: (req, res) => {
-        const productId = req.params.id;
+    getApiDetail: async (req, res) => {
+        try {
+            const productId = req.params.id;
+            const producto = await db.Producto.findByPk(productId)
+            const relacion = await db.Producto.findByPk(productId, { include: [{ association: "aliado_producto" }] })
+            
 
-        db.Producto.findByPk(productId, { include: [{ association: "aliado_producto" }] })
-            .then(producto => {
-                if (!producto) {
-                    return res.status(404).json({
-                        message: "Product not found",
-                        status: 404
-                    });
-                }
-
-                return res.status(200).json({
-                    data: producto,
-                    status: 200
-                });
-            })
-            .catch(error => {
-                return res.status(500).json({
-                    message: "Error fetching product details",
-                    error: error.message,
-                    status: 500
-                });
+            return res.status(200).json({
+                data: producto,
+                aliado: [relacion.aliado_producto],
+                imageUrl: producto.image,
+                status: 200
             });
+
+        } catch (error) {
+            return res.status(500).json({
+                message: "Error fetching product details",
+                error: error.message,
+                status: 500
+            });
+        };
     }
 }
 
